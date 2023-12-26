5aba15df73436c8462d5db55a9928bfdb50884c4
최정윤 <ej921004@naver.com>
2023-12-27 05:16:00 +0900

    Feat : set default page settings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
 export default function Home() {
-  return <main text-center>GYG과 함께 16일째 초록을 키우는 중!!</main>;
+  return (
+    <main className="text-center">
+      <h1>메인페이지</h1>
+      <h1>메인페이지</h1>
+      <h1>메인페이지</h1>
+      <h1>메인페이지</h1>
+      <h1>메인페이지</h1>
+      <h1>메인페이지</h1>
+      <h1>메인페이지</h1>
+      <h1>메인페이지</h1>
+      <h1>메인페이지</h1>
+    </main>
+  );
 }
